9376ff8b1e8e1c1b15fe6df81f9215a21c6f7d74
ДИАНА <GOODFEELING4303@GMAIL.COM>
2018-04-16 12:26:47 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string UserInput;
+            double InputValue;
+            Console.WriteLine("Температра в градусах по Цельсию");
+
+            UserInput = Console.ReadLine();
+            InputValue = double.Parse(UserInput);
+            double ComputedValue = InputValue * 1.8 + 32;
+            Console.WriteLine("C" + InputValue);
+            Console.WriteLine("F" + ComputedValue);
+            Console.WriteLine();
+        }
+    }
+}
